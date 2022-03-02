0740b41461b755e9e68de0e371736e1c2fa73b41
alex-streza <alex.streza@okapistudio.com>
2022-03-02 10:24:18 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import Presale from "components/rounds/Presale";
-import { Fragment } from "react";
 import NavBar from "./components/navbar";
 import Particles from "react-tsparticles";
 import particlesConfig from "./particlesConfig.json";
+import { DappUI, DappProvider } from "@elrondnetwork/dapp-core";
+import "@elrondnetwork/dapp-core/build/index.css";
+import { Fragment } from "react";
+
+const environment = "devnet";
+
+const {
+	TransactionsToastList,
+	SignTransactionsModals,
+	NotificationModal,
+	DappCorePages: { UnlockPage },
+} = DappUI;
 
 const App = () => {
 	return (
-		<Fragment>
-			<NavBar />
-			<main className="container">
-				<Presale />
-				<div className="top-0 bottom-0 z-0 h-screen" style={{ position: "absolute" }}>
-					{/* @ts-ignore */}
-					<Particles id="tsparticles" options={particlesConfig} height="100vh" />
-				</div>
-			</main>
-		</Fragment>
+		<DappProvider
+			environment={environment}
+			customNetworkConfig={{ name: "customConfig", apiTimeout: 6000 }}
+			completedTransactionsDelay={200}>
+			<Fragment>
+				<NavBar />
+				<main className="container">
+					<Presale />
+					<div className="top-0 bottom-0 z-0 h-screen" style={{ position: "absolute" }}>
+						{/* @ts-ignore */}
+						<Particles id="tsparticles" options={particlesConfig} height="100vh" />
+					</div>
+				</main>
+			</Fragment>
+		</DappProvider>
 	);
 };
 
