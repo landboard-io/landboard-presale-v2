3a6aed1fc9737f3d6a56757637a36565a81b9c04
alex-streza <alex.streza@okapistudio.com>
2022-03-15 17:47:51 +0200

    changes

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,6 @@
-export const contractAddress = "erd1qqqqqqqqqqqqqpgqljx7ahxgz60k5tn5h7rs94z9ukwudvgrstrqz3lhny";
+export const contractAddress =
+  "erd1qqqqqqqqqqqqqpgqljx7ahxgz60k5tn5h7rs94z9ukwudvgrstrqz3lhny";
 export const dAppName = "LandBoard Presale";
 
 export const whitelistDate = 1647280800000;
-export const presaleDate = 1647367200000;
+export const presaleDate = 1647359700000; // 1647367200000;
