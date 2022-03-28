ba9045c9c4ab041129ac3767eb4de0e6247e39a8
alex-streza <alex.streza@okapistudio.com>
2022-03-29 00:14:06 +0300

    changes

--- src/components/cards/BuyCard.tsx
+++ src/components/cards/BuyCard.tsx
@@ -13,203 +13,147 @@ import toast from "react-hot-toast";
 import whitelistedAddresses from "./data.json";
 
 const variants = {
-  hidden: {
-    opacity: 0,
-    scale: 0,
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-  },
+	hidden: {
+		opacity: 0,
+		scale: 0,
+	},
+	visible: {
+		opacity: 1,
+		scale: 1,
+	},
 };
 
 const conversionRate = 0.0003;
 const contractAddress =
-  process.env.REACT_APP_ELROND_NETWORK === "mainnet"
-    ? mainnetContractAddress
-    : devnetContractAddress;
+	process.env.REACT_APP_ELROND_NETWORK === "mainnet" ? mainnetContractAddress : devnetContractAddress;
 const environment =
-  process.env.REACT_APP_ELROND_NETWORK === "mainnet"
-    ? ""
-    : process.env.REACT_APP_ELROND_NETWORK + "-";
+	process.env.REACT_APP_ELROND_NETWORK === "mainnet" ? "" : process.env.REACT_APP_ELROND_NETWORK + "-";
 
 const LabelButton = (props: any) => (
-  <button
-    type="button"
-    className="flex px-2 py-1 text-sm font-bold uppercase rounded bg-purple-darker text-purple"
-    {...props}
-  >
-    Max
-  </button>
+	<button
+		type="button"
+		className="flex px-2 py-1 text-sm font-bold uppercase rounded bg-purple-darker text-purple"
+		{...props}>
+		Max
+	</button>
 );
 
 const BuyCard = () => {
-  const { address, account, ...rest } = useGetAccountInfo();
+	const { address, account, ...rest } = useGetAccountInfo();
 
-  const [totalLandBalance, setTotalLandBalance] = useState(0);
-  const [egldPrice, setEgldPrice] = useState(0);
-  const [egldAmount, setEgldAmount] = useState("0");
-  const [landAmount, setLandAmount] = useState("0");
-  const isWhitelisted = useMemo(
-    () =>
-      whitelistedAddresses.data.some(
-        (waddress: any) => waddress.address.trim() === address.trim()
-      ),
-    [address]
-  );
+	const [totalLandBalance, setTotalLandBalance] = useState(0);
+	const [egldPrice, setEgldPrice] = useState(0);
+	const [egldAmount, setEgldAmount] = useState("0");
+	const [landAmount, setLandAmount] = useState("0");
+	const isWhitelisted = useMemo(
+		() => whitelistedAddresses.data.some((waddress: any) => waddress.address.trim() === address.trim()),
+		[address]
+	);
 
-  const { timeLeft } = useTimeUntilLaunch(isWhitelisted);
+	const { timeLeft } = useTimeUntilLaunch(isWhitelisted);
 
-  const handleChangeEgldAmount = (e: any) => {
-    const regex = RegExp(/[0-9]+/g);
-    const test_result = regex.test(e.target.value);
+	const handleChangeEgldAmount = (e: any) => {
+		const regex = RegExp(/[0-9]+/g);
+		const test_result = regex.test(e.target.value);
 
-    if ((test_result && e.target.value <= 1) || e.target.value == "") {
-      setEgldAmount(e.target.value);
-      setLandAmount((e.target.value / conversionRate).toFixed(4));
-    }
-  };
+		if ((test_result && e.target.value <= 1) || e.target.value == "") {
+			setEgldAmount(e.target.value);
+			setLandAmount((e.target.value / conversionRate).toFixed(4));
+		}
+	};
 
-  const handleChangeLandAmount = (e: any) => {
-    const regex = RegExp(/[0-9]+/g);
-    const test_result = regex.test(e.target.value);
+	const handleChangeLandAmount = (e: any) => {
+		const regex = RegExp(/[0-9]+/g);
+		const test_result = regex.test(e.target.value);
 
-    if ((test_result && e.target.value <= 3333.3333) || e.target.value == "") {
-      setLandAmount(e.target.value);
-      setEgldAmount((e.target.value * conversionRate).toFixed(4));
-    }
-  };
+		if ((test_result && e.target.value <= 3333.3333) || e.target.value == "") {
+			setLandAmount(e.target.value);
+			setEgldAmount((e.target.value * conversionRate).toFixed(4));
+		}
+	};
 
-  const buyToken = async (e: any) => {
-    e.preventDefault();
-    const tx = {
-      value: Balance.egld(egldAmount),
-      data: "buy",
-      receiver: contractAddress,
-    };
-    await refreshAccount();
-    const transaction = await sendTransactions({
-      transactions: tx,
-    });
-    toast("Sending transaction.");
-    toast("You are buying " + landAmount + " LAND");
-  };
+	const buyToken = async (e: any) => {
+		e.preventDefault();
+		const tx = {
+			value: Balance.egld(egldAmount),
+			data: "buy",
+			receiver: contractAddress,
+		};
+		await refreshAccount();
+		const transaction = await sendTransactions({
+			transactions: tx,
+		});
+		toast("Sending transaction.");
+		toast("You are buying " + landAmount + " LAND");
+	};
 
-  useEffect(() => {
-    axios
-      .get(`https://${environment}api.elrond.com/economics`)
-      .then((res: any) => {
-        setEgldPrice(res.data.price);
-      });
-  }, []);
+	useEffect(() => {
+		axios.get(`https://${environment}api.elrond.com/economics`).then((res: any) => {
+			setEgldPrice(res.data.price);
+		});
+	}, []);
 
-  useEffect(() => {
-    if (account.address != "") {
-      axios
-        .get(
-          `https://${environment}api.elrond.com/accounts/${account.address}/tokens`
-        )
-        .then((res: any) => {
-          if (res.data?.length > 0) {
-            const tokens = res.data.filter(
-              (a: any) =>
-                a?.identifier === "LAND-40f26f" || a?.ticker === "LAND-40f26f"
-            );
-            setTotalLandBalance(
-              tokens.length > 0 ? tokens[0].balance / 10 ** 18 : 0
-            );
-          }
-        });
-    }
-  }, [account]);
+	useEffect(() => {
+		if (account.address != "") {
+			axios.get(`https://${environment}api.elrond.com/accounts/${account.address}/tokens`).then((res: any) => {
+				if (res.data?.length > 0) {
+					const tokens = res.data.filter((a: any) => a?.identifier === "LAND-40f26f" || a?.ticker === "LAND-40f26f");
+					setTotalLandBalance(tokens.length > 0 ? tokens[0].balance / 10 ** 18 : 0);
+				}
+			});
+		}
+	}, [account]);
 
-  const totalEgldBalance = useMemo(
-    () => (account ? parseInt(account.balance) / 10 ** 18 : 0).toFixed(4),
-    [account]
-  );
-  const disabled =
-    egldAmount === "0" ||
-    landAmount === "0" ||
-    !egldAmount ||
-    !landAmount ||
-    !address;
+	const totalEgldBalance = useMemo(() => (account ? parseInt(account.balance) / 10 ** 18 : 0).toFixed(4), [account]);
+	const disabled = egldAmount === "0" || landAmount === "0" || !egldAmount || !landAmount || !address;
 
-  const accountEgldBalance = parseFloat(totalEgldBalance);
+	const accountEgldBalance = parseFloat(totalEgldBalance);
 
-  return (
-    <motion.div variants={variants} className="pb-10 card">
-      <h2 className="mb-10">Buy LAND Token</h2>
-      <form className="flex flex-col gap-5 text-left" onSubmit={buyToken}>
-        <Input
-          label="YOU BUY"
-          tag="LAND"
-          value={landAmount}
-          onChange={handleChangeLandAmount}
-        />
-        {address && (
-          <span className="tiny-label">You have {totalLandBalance} LAND</span>
-        )}
-        <Input
-          label="YOU PAY"
-          tag="EGLD"
-          value={egldAmount}
-          onChange={handleChangeEgldAmount}
-          LabelButton={
-            <LabelButton
-              onClick={() =>
-                handleChangeEgldAmount({
-                  target: {
-                    value:
-                      accountEgldBalance < 1
-                        ? accountEgldBalance > 0.0075
-                          ? (accountEgldBalance - 0.0075).toFixed(4)
-                          : 0
-                        : 1,
-                  },
-                })
-              }
-            />
-          }
-        />
-        {address && (
-          <p className="tiny-label">
-            <span>Minimum buy 0.2 $EGLD</span>
-            <span>You have {totalEgldBalance} EGLD</span>
-            <span>
-              You will receive 20% unlocked and 80% locked tokens (
-              <a href="https://twitter.com/landboard_io/status/1494239023094767619?s=21">
-                see vesting period here
-              </a>
-              )
-            </span>
-          </p>
-        )}
-
-        {!isWhitelisted && address && (
-          <span className="-mb-8 text-sm font-bold">
-            You are not whitelisted. The list updates periodically.
-          </span>
-        )}
-        {isWhitelisted && (
-          <span className="-mb-8 text-sm font-bold text-purple">
-            You are whitelisted!
-          </span>
-        )}
-        <Button
-          className="w-full filled"
-          containerClassname="mt-5"
-          type="submit"
-          disabled={disabled}
-          hideComingSoon={!isWhitelisted || !address}
-        >
-          BUY $LAND
-        </Button>
-        <span className="text-xs font-bold">
-          1 EGLD= ${egldPrice} | 1 EGLD = 3333 LAND
-        </span>
-      </form>
-    </motion.div>
-  );
+	return (
+		<motion.div variants={variants} className="pb-10 card">
+			<h2 className="mb-10">Buy LAND Token</h2>
+			<form className="flex flex-col gap-5 text-left" onSubmit={buyToken}>
+				<Input label="YOU BUY" tag="LAND" value={landAmount} onChange={handleChangeLandAmount} />
+				{address && <span className="tiny-label">You have {totalLandBalance} LAND</span>}
+				<Input
+					label="YOU PAY"
+					tag="EGLD"
+					value={egldAmount}
+					onChange={handleChangeEgldAmount}
+					LabelButton={
+						<LabelButton
+							onClick={() =>
+								handleChangeEgldAmount({
+									target: {
+										value:
+											accountEgldBalance < 1
+												? accountEgldBalance > 0.0075
+													? (accountEgldBalance - 0.0075).toFixed(4)
+													: 0
+												: 1,
+									},
+								})
+							}
+						/>
+					}
+				/>
+				{address && (
+					<p className="tiny-label">
+						<span>Minimum buy 0.2 $EGLD</span>
+						<span>You have {totalEgldBalance} EGLD</span>
+						<span>
+							You will receive 20% unlocked and 80% locked tokens (
+							<a href="https://twitter.com/landboard_io/status/1494239023094767619?s=21">see vesting period here</a>)
+						</span>
+					</p>
+				)}
+				<Button className="w-full filled" containerClassname="mt-5" type="submit" disabled={disabled} hideComingSoon>
+					BUY $LAND
+				</Button>
+				<span className="text-xs font-bold">1 EGLD= ${egldPrice} | 1 EGLD = 3333 LAND</span>
+			</form>
+		</motion.div>
+	);
 };
 
 export default BuyCard;
